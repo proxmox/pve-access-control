proxmox/pve-access-control

commit 46bfd59dfca655b263d1f905be37d985416717ac

acls: restrict less-privileged ACL modifications
there are currently three possibilities to modify ACLs without the
'Permissions.Modify' privilege in PVE::RPCEnvironment::check_perm_modify:

    if ($path =~ m|^/storage/.+$|) {
	push @$testperms, 'Datastore.Allocate';
    } elsif ($path =~ m|^/vms/.+$|) {
	push @$testperms, 'VM.Allocate';
    } elsif ($path =~ m|^/pool/.+$|) {
	push @$testperms, 'Pool.Allocate';
    }

lock those down by only allowing the currently authenticated user to hand out a
subset of their own privileges, never more.

for example, this still allows a PVEVMAdmin to create ACLs for other
users/tokens with PVEVMUser (on '/vm/XXX'), but not with Administrator or
PVEPermAdmin.

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

src/PVE/API2/ACL.pm
@@ -148,8 +148,11 @@ __PACKAGE__->register_method ({
 
 	PVE::AccessControl::lock_user_config(
 	    sub {
-
 		my $cfg = cfs_read_file("user.cfg");
+
+		my $rpcenv = PVE::RPCEnvironment::get();
+		my $authuser = $rpcenv->get_user();
+		my $auth_user_privs = $rpcenv->permissions($authuser, $path);
 
 		my $propagate = 1;
 
@@ -162,6 +165,25 @@ __PACKAGE__->register_method ({
 		foreach my $role (split_list($param->{roles})) {
 		    die "role '$role' does not exist\n"
 			if !$cfg->{roles}->{$role};
+
+		    if (!$auth_user_privs->{'Permissions.Modify'}) {
+			# 'perm-modify' allows /vms/* with VM.Allocate and similar restricted use cases
+			# filter those to only allow handing out a subset of currently active privs
+		        my $role_privs = $cfg->{roles}->{$role};
+		        my $verb = $param->{delete} ? 'remove' : 'add';
+		        foreach my $priv (keys $role_privs->%*) {
+			    raise_param_exc({ role => "Cannot $verb role '$role' - requires 'Permissions.Modify' or superset of privileges." })
+				if !defined($auth_user_privs->{$priv});
+
+			    # propagation is only potentially problematic for adding ACLs, not removing..
+			    raise_param_exc({ role => "Cannot $verb role '$role' with propagation - requires 'Permissions.Modify' or propagated superset of privileges." })
+				if $propagate && $auth_user_privs->{$priv} != $propagate && !$param->{delete};
+		        }
+
+			# NoAccess has no privs, needs an explicit check
+		        raise_param_exc({ role => "Cannot $verb role '$role' - requires 'Permissions.Modify'"})
+			    if $role eq 'NoAccess';
+		    }
 
 		    foreach my $group (split_list($param->{groups})) {
 

commit 7d05a239d29ea2402aab7f727e81c0588f46b45d

api: ACL update: fix handling of Permissions.Modify
With the PVE 8.0 major release, the scope of
non-"Permissions.Modify"-based ACL update privileges were reduced (so
that users with for example, VM.Allocate on a VM could only delegate
their own privileges, but not arbitrary other ones). that additional
logic had a wrong guard and was accidentally triggered for calls where
the user had the "Permissions.Modify" privilege on the modified ACL
path, but without propagation set.

A user with "Permissions.Modify" on a path should be able to set
arbitrary ACLs for that path, even without propagation.

Reported on the forum: https://forum.proxmox.com/threads/151032/

Fixes: 46bfd59 ("acls: restrict less-privileged ACL modifications")
Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

src/PVE/API2/ACL.pm
@@ -166,7 +166,8 @@ __PACKAGE__->register_method ({
 		    die "role '$role' does not exist\n"
 			if !$cfg->{roles}->{$role};
 
-		    if (!$auth_user_privs->{'Permissions.Modify'}) {
+		    # permissions() returns set privs as key, and propagate bit as value!
+		    if (!defined($auth_user_privs->{'Permissions.Modify'})) {
 			# 'perm-modify' allows /vms/* with VM.Allocate and similar restricted use cases
 			# filter those to only allow handing out a subset of currently active privs
 		        my $role_privs = $cfg->{roles}->{$role};