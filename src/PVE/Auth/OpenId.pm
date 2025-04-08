proxmox/pve-access-control

commit e80f840ccc3c469b05cd34d9a9987de293862c93

openid: make groups-claim RE more restrictive
always possible to lift, but hard to lock down after the fact..

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

src/PVE/Auth/OpenId.pm
@@ -9,8 +9,8 @@ use PVE::Cluster qw(cfs_register_file cfs_read_file cfs_write_file cfs_lock_file
 
 use base qw(PVE::Auth::Plugin);
 
-# include all printable ascii characters
-my $openid_claim_regex = qr/[ -~]+/;
+# FIXME: restrict username-claim as well?
+my $openid_claim_regex = qr/A-Za-z0-9\.\-_/;
 
 sub type {
     return 'openid';

commit 55ab21ecfcd090ef7d364be39bdbb740391138a4

openid: fix groups-claim regex
The previous regex matched exactly that combination of characters,
rather than any combination of the specified ones.

Fixes: e80f840 ("openid: make groups-claim RE more restrictive")
Signed-off-by: Mira Limbeck <m.limbeck@proxmox.com>
Link: https://lore.proxmox.com/20250408113349.165831-1-m.limbeck@proxmox.com

src/PVE/Auth/OpenId.pm
@@ -10,7 +10,7 @@ use PVE::Cluster qw(cfs_register_file cfs_read_file cfs_write_file cfs_lock_file
 use base qw(PVE::Auth::Plugin);
 
 # FIXME: restrict username-claim as well?
-my $openid_claim_regex = qr/A-Za-z0-9\.\-_/;
+my $openid_claim_regex = qr/[A-Za-z0-9\.\-_]+/;
 
 sub type {
     return 'openid';